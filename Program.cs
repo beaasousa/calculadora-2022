793bbda5c4adae06208bfdd92b7a78dcef12fb16
Ana Beatriz Costa de Sousa <anacostasousa08@gmail.com>
2022-05-06 20:38:23 -0300

    finaliza exercicio

--- Program.cs
+++ Program.cs
@@ -20,29 +20,16 @@ switch (opcaoDesejada)
 
         break;
     case "-":
-        Console.WriteLine("--- Subtração ---");
+        CalcularSubtracao();
+    
         break;
     case "*":
-        double a, b, resultadoMulti;
-
-        Console.WriteLine("--- Multiplicação A*B ---\n");
-        Console.WriteLine("Digite os valores.");
-
-        Console.Write("A = ");
-        a = Convert.ToDouble(Console.ReadLine());
-        Console.Write("B = ");
-        b = Convert.ToDouble(Console.ReadLine());
-
-        resultadoMulti = a*b;
-
-        Console.WriteLine($"{a} multiplicado por {b} é {resultadoMulti}");
-
-        Console.WriteLine("\nPressione uma tecla para continuar...");
-        Console.ReadKey();
+       CalcularMultiplicacao();
 
         break;
     case "/":
-        Console.WriteLine("Você selecionou divisão");
+      CalcularDivisao();
+      
         break;
     default:
         Console.WriteLine("Opção inválida.");
@@ -53,6 +40,95 @@ Console.WriteLine("\nObrigado por utilizar nosso programa.");
 
 void CalcularSoma()
 {
+    double a, b, resultado;
+
     Console.WriteLine("--- Soma A+B ---\n");
+    Console.WriteLine("Digite os valores.");
+
+    Console.Write("A = ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("B = ");
+    b = Convert.ToDouble(Console.ReadLine());
+
+    resultado = a + b;
+
+    Console.WriteLine($"{a} somado com {b} é {resultado}");
+
+    Console.WriteLine("\nPressione uma tecla para continuar...");
+    Console.ReadKey();
+
+    return;
+}
+
+void CalcularSubtracao()
+{
+    double a, b, resultado;
+
+    Console.WriteLine("--- Subtração A-B ---\n");
+    Console.WriteLine("Digite os valores.");
+
+    Console.Write("A = ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("B = ");
+    b = Convert.ToDouble(Console.ReadLine());
+
+    resultado = a - b;
+
+    Console.WriteLine($"{a} subtraindo {b} é {resultado}");
+
+    Console.WriteLine("\nPressione uma tecla para continuar...");
+    Console.ReadKey();
+
+    return;
+}
+
+void CalcularMultiplicacao()
+{
+    double a, b, resultado;
+
+    Console.WriteLine("--- Multiplicação A*B ---\n");
+    Console.WriteLine("Digite os valores.");
+
+    Console.Write("A = ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("B = ");
+    b = Convert.ToDouble(Console.ReadLine());
+
+    resultado = a * b;
+
+    Console.WriteLine($"{a} multiplicado por {b} é {resultado}");
+
+    Console.WriteLine("\nPressione uma tecla para continuar...");
+    Console.ReadKey();
+
+    return;
+}
+
+void CalcularDivisao()
+{
+    double a, b, resultado;
+
+    Console.WriteLine("--- Divisão A/B ---\n");
+    Console.WriteLine("Digite os valores.");
+
+    Console.Write("A = ");
+    a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("B = ");
+    b = Convert.ToDouble(Console.ReadLine());
+
+    if (b == 0)
+    {
+        Console.WriteLine("\nNão é possível dividir por zero.");
+    }
+    else
+    {
+        resultado = a / b;
+
+        Console.WriteLine($"{a} dividido por {b} é {resultado}");
+    }
+
+    Console.WriteLine("\nPressione uma tecla para continuar...");
+    Console.ReadKey();
+
     return;
 }
